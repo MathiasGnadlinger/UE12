83aaf213c4fa63cb3f5a13e429acdc32e38fd51e
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2021-01-26 15:07:51 +0100

    Model_programmiert

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,58 +1,137 @@
 package model;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Model
 {
     private ModularCounter red;
     private ModularCounter green;
     private ModularCounter blue;
+    private String hex;
+
+    @FXML private TextField tf_RED_Input;
+    @FXML private TextField tf_GREEN_Input;
+    @FXML private TextField tf_BLUE_Input;
+    @FXML private TextField tf_HEX_Output;
+    @FXML private Button btn_Color_Output;
 
 
-    public Model(ModularCounter red, ModularCounter green, ModularCounter blue) {
+    public Model(ModularCounter red, ModularCounter green, ModularCounter blue)
+    {
         this.red = red;
         this.green = green;
         this.blue = blue;
     }
 
-    public ModularCounter getRed() {
-        return red;
-    }
-
-    public ModularCounter getGreen() {
-        return green;
-    }
-
-    public ModularCounter getBlue() {
-        return blue;
-    }
-
-    public String getHex()
+    public int getRed()
     {
-        return getHex();
+        return red.getValue();
     }
 
+    public int getGreen()
+    {
+        return green.getValue();
+    }
 
+    public int getBlue()
+    {
+        return blue.getValue();
+    }
+
+    public String getHex(String hex)
+    {
+        int red = Integer.parseInt(tf_RED_Input.getText());
+        String hexRed = Integer.toHexString(red);
+        int green = Integer.parseInt(tf_GREEN_Input.getText());
+        String hexGreen = Integer.toHexString(green);
+        int blue = Integer.parseInt(tf_BLUE_Input.getText());
+        String hexBlue = Integer.toHexString(blue);
+
+        hex = hexRed + hexGreen + hexBlue;
+        System.out.printf(" Hexadezimal: %c%c%c ", hexRed,hexGreen,hexBlue);
+
+        return hex;
+    }
 
 
     public void changeColorViaAbsoluteValue(ColorCode colorCode, String value)
     {
-
+        int intValue = Integer.parseInt(value);
+        switch (colorCode)
+        {
+            case RED:
+                red.reset();
+                red.update(intValue);
+                break;
+            case GREEN:
+                green.reset();
+                green.update(intValue);
+                break;
+            case BLUE:
+                blue.reset();
+                blue.update(intValue);
+                break;
+        }
     }
 
     public void changeColorViaAbsoluteValue(ColorCode colorCode, int value)
     {
-
+        switch (colorCode)
+        {
+            case RED:
+                red.reset();
+                red.update(value);
+                break;
+            case GREEN:
+                green.reset();
+                green.update(value);
+                break;
+            case BLUE:
+                blue.reset();
+                blue.update(value);
+                break;
+        }
     }
+
 
     public void changeColorViaRelativeValue(ColorCode colorCode, String value)
     {
+        int intValue = Integer.parseInt(value);
+        switch (colorCode) {
+            case RED:
+                red.update(intValue);
+                break;
+
+            case GREEN:
+                green.update(intValue);
+                break;
+
+            case BLUE:
+                blue.update(intValue);
+                break;
+        }
 
     }
 
     public void changeColorViaRelativeValue(ColorCode colorCode, int value)
     {
+        switch (colorCode) {
+            case RED:
+                red.update(value);
+                break;
+
+            case GREEN:
+                green.update(value);
+                break;
+
+            case BLUE:
+                blue.update(value);
+                break;
+        }
 
     }
-
 
 
     public static void main(String[] args)
@@ -61,7 +140,8 @@ public class Model
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Model" +
                 "red = " + red +
                 ", green = " + green +
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller
+{
+    @FXML private TextField tf_RED_Input;
+    @FXML private TextField tf_GREEN_Input;
+    @FXML private TextField tf_BLUE_Input;
+    @FXML private TextField tf_HEX_Output;
+
+    @FXML private Button btn_Color_Output;
+    @FXML private Button btn_REDpl;
+    @FXML private Button btn_REDmi;
+    @FXML private Button btn_GREENpl;
+    @FXML private Button btn_GREENmi;
+    @FXML private Button btn_BLUEpl;
+    @FXML private Button btn_BLUEmi;
+
+
+
+
+
+
+
+
 }
