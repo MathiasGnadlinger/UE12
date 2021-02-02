4e7f7e46407db6f6389e12de12803ecb0a181290
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2021-02-02 10:29:55 +0100

    Loader und Writer implementiert

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.ColorCode;
 import model.Model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * @author Mathias Gnadlinger
  * @version 12, 26.01.2021
@@ -20,6 +26,9 @@ public class Controller
     @FXML private TextField tf_HEX_Output;
 
     @FXML private Button btn_Color_Output;
+
+    private final String FILENAME = "Color.dat";
+    private final String FILEFORMAT = "Color File Format 1.0";
 
 
     @FXML
@@ -55,7 +64,7 @@ public class Controller
     public void SubGreen()
     {
         model.changeColorViaRelativeValue(ColorCode.GREEN,-10);
-        tf_BLUE_Input.setText(Integer.toString(model.getGreen()));
+        tf_GREEN_Input.setText(Integer.toString(model.getGreen()));
         ShowHex();
     }
 
@@ -90,16 +99,65 @@ public class Controller
     }
 
 
-    @FXML
-    private void ShowHex()
+    @FXML private void ShowHex()
     {
         tf_HEX_Output.setText(model.getHex());
         btn_Color_Output.setStyle("-fx-background-color: " + model.getHex() + ";");
     }
 
-    @FXML
-    public void initialize()
+    @FXML public void initialize()
     {
         model = new Model();
+    }
+
+    public boolean loadFromFile()
+    {
+        boolean issaved = false;
+        try
+        {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("Color.dat"));
+            if (bufferedReader.readLine().equals("Color File Format 1.0"))
+            {
+                model.changeColorViaAbsoluteValue(ColorCode.RED, bufferedReader.readLine());
+                tf_RED_Input.setText(Integer.toString(model.getRed()));
+
+                model.changeColorViaAbsoluteValue(ColorCode.GREEN, bufferedReader.readLine());
+                tf_GREEN_Input.setText(Integer.toString(model.getGreen()));
+
+                model.changeColorViaAbsoluteValue(ColorCode.BLUE, bufferedReader.readLine());
+                tf_BLUE_Input.setText(Integer.toString(model.getBlue()));
+                ShowHex();
+
+                issaved = true;
+            }
+        }
+        catch(IOException ex)
+        {
+            System.out.printf("Error while Loading");
+        }
+        return issaved;
+    }
+
+    public boolean saveToFile()
+    {
+        boolean issaved = false;
+        try
+        {
+            FileWriter fw= new FileWriter("Color.dat");
+            fw.write("Color File Format 1.0");
+            fw.write(System.lineSeparator());
+            fw.write(Integer.toString(model.getRed()));
+            fw.write(System.lineSeparator());
+            fw.write(Integer.toString(model.getGreen()));
+            fw.write(System.lineSeparator());
+            fw.write(Integer.toString(model.getBlue()));
+            issaved = true;
+            fw.close();
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        return issaved;
     }
 }
